c1bda4d2101778737288ed1f05b8e3e561755e3d
Daniele Ceribelli <danieleceribelli@gmail.com>
2024-10-31 12:35:15 +0100

    Exercise 02_07 completed

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut restult = 1;
+    for i in 1..=n {
+        restult *= i;
+    }
+    restult
 }
 
 #[cfg(test)]
